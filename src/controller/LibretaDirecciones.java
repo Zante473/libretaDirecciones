b7fa727eb900ab648e521a5d538b228b2fe5c63f
Daniel Zamarreño <>
2018-01-09 17:07:37 +0100

    He añadido LibretaDirecciones.java

---
+++ src/controller/LibretaDirecciones.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author dam
+ */
+public class LibretaDirecciones extends Application {
+    
+    private Stage escenarioPrincipal;
+    private BorderPane layoutPrincipal;
+    private AnchorPane vistaPersona;
+    
+    @Override
+    public void start(Stage escenarioPrincipal) {
+        
+        //Escenario en el que cargo las diferentes escenas (vistas)
+        this.escenarioPrincipal = escenarioPrincipal;
+        
+        //Establezco el título
+        this.escenarioPrincipal.setTitle("Libreta de direcciones");
+        
+        //Inicializo el layout principal
+        initLayoutPrincipal();
+        
+        //Muestro la vista persona
+        muestraVistaPersona();
+       
+    }
+
+    public void initLayoutPrincipal(){
+        
+        //Cargo el layout principal a partir de la vista vistaPrincipal.fxml
+        
+        FXMLLoader loader = new FXMLLoader();
+        URL location = LibretaDirecciones.class.getResource("../view/vistaPrincipal.fxml");
+        loader.setLocation(location);
+        
+        try {
+            layoutPrincipal = loader.load();
+        } catch (IOException ex) {
+            
+        }
+        
+        //Cargo y muestro la escena que contiene ese layout principal
+        
+        Scene escena = new Scene(layoutPrincipal);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+        
+    }
+    
+    public void muestraVistaPersona(){
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}

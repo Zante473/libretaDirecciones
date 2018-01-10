281200829e89c4f9a5db7a7b10e03e8bb957fb79
Daniel Zamarreño <dam@5300101A11119>
2018-01-10 20:13:16 +0100

    He añadido LibretaDirecciones.java

--- src/controller/LibretaDirecciones.java
+++ src/controller/LibretaDirecciones.java
@@ -20,57 +20,72 @@ import javafx.stage.Stage;
  * @author dam
  */
 public class LibretaDirecciones extends Application {
-    
+
     private Stage escenarioPrincipal;
     private BorderPane layoutPrincipal;
     private AnchorPane vistaPersona;
-    
+
     @Override
     public void start(Stage escenarioPrincipal) {
-        
+
         //Escenario en el que cargo las diferentes escenas (vistas)
         this.escenarioPrincipal = escenarioPrincipal;
-        
+
         //Establezco el título
         this.escenarioPrincipal.setTitle("Libreta de direcciones");
-        
+
         //Inicializo el layout principal
         initLayoutPrincipal();
-        
+
         //Muestro la vista persona
         muestraVistaPersona();
-       
+
     }
 
-    public void initLayoutPrincipal(){
-        
+    public void initLayoutPrincipal() {
+
         //Cargo el layout principal a partir de la vista vistaPrincipal.fxml
-        
         FXMLLoader loader = new FXMLLoader();
         URL location = LibretaDirecciones.class.getResource("../view/vistaPrincipal.fxml");
         loader.setLocation(location);
-        
+
         try {
             layoutPrincipal = loader.load();
         } catch (IOException ex) {
-            
+
         }
-        
+
         //Cargo y muestro la escena que contiene ese layout principal
-        
         Scene escena = new Scene(layoutPrincipal);
         escenarioPrincipal.setScene(escena);
         escenarioPrincipal.show();
-        
+
     }
-    
-    public void muestraVistaPersona(){
-        
-        
+
+    public void muestraVistaPersona() {
+
+        //Cargo la vista persona a partir de la vista vistaPersona.fxml
+        FXMLLoader loader = new FXMLLoader();
+        URL location = LibretaDirecciones.class.getResource("../view/VistaPersona.fxml");
+        loader.setLocation(location);
+        try {
+            vistaPersona = loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(LibretaDirecciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //Añado al centro del layoutPrincipal
+        layoutPrincipal.setCenter(vistaPersona);
+
     }
-    
+
+    //Invoco el método getPrimaryStage para que devuelva mi escenario principal
+    public Stage getPrimaryStage() {
+        return escenarioPrincipal;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
